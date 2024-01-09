98cf4a4aa013da676ab838ac7fa14cb5eb6241a6
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-01-10 00:09:42 +0600

    Added Phone Card

--- src/Components/PhoneCollections/PhoneCollections.jsx
+++ src/Components/PhoneCollections/PhoneCollections.jsx
@@ -6,8 +6,8 @@ const PhoneCollections = () => {
     console.log(phones);
     return (
         <div>
-            <h2 className="text-4xl font-bold">Our Phones Collections</h2>
-            <div>
+            <h2 className="text-4xl font-bold text-center mt-32 mb-24">Our Phones Collections</h2>
+            <div className="grid grid-cols-4 gap-5">
                 {
                     phones.map(phone => <PhonesCard key={phone.id} phone={phone}></PhonesCard>)
                 }
--- src/Components/PhonesCard/PhonesCard.jsx
+++ src/Components/PhonesCard/PhonesCard.jsx
@@ -1,7 +1,46 @@
-const PhonesCard = ({phone}) => {
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
+const PhonesCard = ({ phone }) => {
+    const { image, phone_name, brand_name, price, rating } = phone
     return (
         <div>
-            
+            <Card className="">
+                <CardHeader shadow={false} floated={false} className="h-96">
+                    <img
+                        src={image}
+                        className="h-full w-full object-cover"
+                    />
+                </CardHeader>
+                <CardBody>
+                    <Typography color="blue-gray" className="font-bold">
+                        {brand_name}
+                    </Typography>
+                    <div className="mb-2 flex items-center justify-between">
+                        <Typography color="blue-gray" className="font-medium">
+                            {phone_name}
+                        </Typography>
+                        <Typography color="blue-gray" className="font-medium">
+                            {price}
+                        </Typography>
+                    </div>
+                </CardBody>
+                <CardFooter className="pt-0">
+                    <Button
+                        ripple={false}
+                        fullWidth={true}
+                        className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    >
+                        See Details
+                    </Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 };
